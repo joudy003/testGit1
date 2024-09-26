513cb803f9237ba10044c05f87aba0d0884d495b
joudy003 <joudy.j.t@gmail.com>
2024-09-26 17:05:40 +0300

    Initial Commit

---
+++ testGit1/testGit1App.swift
@@ -0,0 +1,17 @@
+//
+//  testGit1App.swift
+//  testGit1
+//
+//  Created by Joud Abdullah AlAmri on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct testGit1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
